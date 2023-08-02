a7d6e9246d3ab3685efcb6c7c0c1159f48e24925
Moolmancell <richmondclaudebodot@gmail.com>
2023-08-02 21:59:31 +0800

    Add number of pixel input

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const canvas = document.querySelector(".canvas");
+const pixelNo = document.getElementById("pixelNumber");
 let canvasWidth = canvas.offsetWidth;
 let canvasHeight = canvas.offsetHeight;
 
-let gridNo = 10;
+const pixels = document.querySelectorAll("div.row")
+
+let grid = spawnGrid(16);
+
+pixelNo.addEventListener("input", (e) => {
+    canvas.innerHTML = "";
+    spawnGrid(pixelNo.value)
+})
 
 /*---------------------------------*/
-for (let i = 1; i <= gridNo; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column")
-    column.style.height = `${canvasHeight/gridNo}px`;
-    canvas.appendChild(column);
+function spawnGrid(gridNo) {
+    if (gridNo < 2) {
+        gridNo = 2;
+    } else if (gridNo > 64) {
+        gridNo = 64;
+    }
+
     for (let i = 1; i <= gridNo; i++) {
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.style.height = `${canvasHeight/gridNo}px`;
-        row.style.width = `${canvasWidth/gridNo}px`;
-        column.appendChild(row);
+        const column = document.createElement("div");
+        column.classList.add("column")
+        column.style.height = `${canvasHeight/gridNo}px`;
+        canvas.appendChild(column);
+        for (let i = 1; i <= gridNo; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            row.style.height = `${canvasHeight/gridNo}px`;
+            row.style.width = `${canvasWidth/gridNo}px`;
+            column.appendChild(row);
+        }
     }
 }
