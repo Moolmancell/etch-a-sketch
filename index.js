c76f1a38b4052830c03754bfd4ed5a4de91904c9
Moolmancell <richmondclaudebodot@gmail.com>
2023-08-03 11:46:19 +0800

    Add color picking and a reset button

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const body = document.querySelector("body");
 const canvas = document.querySelector(".canvas");
 const pixelNo = document.getElementById("pixelNumber");
 let canvasWidth = canvas.offsetWidth;
 let canvasHeight = canvas.offsetHeight;
-
-const pixels = document.querySelectorAll("div.row")
-
+const black = document.getElementById("black");
+const RGB = document.getElementById("RGB")
+const eraser = document.getElementById("eraser")
+const resetButton = document.getElementById("eraseButton")
+let pixels;
 let grid = spawnGrid(16);
 
 pixelNo.addEventListener("input", (e) => {
@@ -13,6 +16,25 @@ pixelNo.addEventListener("input", (e) => {
 })
 
 /*---------------------------------*/
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
+
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r,g,b];
+}
+
+function randomHexColor() {
+    let [r,g,b] =randomRgbColor();
+    let hr = r.toString(16).padStart(2, '0');
+    let hg = g.toString(16).padStart(2, '0');
+    let hb = b.toString(16).padStart(2, '0');
+    return "#" + hr + hg + hb;
+}
+
 function spawnGrid(gridNo) {
     if (gridNo < 2) {
         gridNo = 2;
@@ -33,4 +55,23 @@ function spawnGrid(gridNo) {
             column.appendChild(row);
         }
     }
+    pixels = canvas.querySelectorAll("div.row")
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mouseover", (e) => {
+            if (black.checked === true) {
+                e.target.style.backgroundColor = "black";
+            } else if (RGB.checked === true){
+                e.target.style.backgroundColor = `${randomHexColor()}`;
+            } else if (eraser.checked === true) {
+                e.target.style.backgroundColor = "";
+            }
+           
+        })
+       });
 }
+
+resetButton.addEventListener("click", () => {
+    pixels.forEach(pixel => {
+        pixel.style.backgroundColor = "";
+    });
+})
